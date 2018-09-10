fd896cb55d11e48cfe9d731882b03e86adbb6f90
Cory Taylor <coryandrewtaylor@gmail.com>
2018-09-10 12:03:03 -0500

    Delete node_modules folder
    node_modules accidentally got committed. Delete it to remove some noise from the repo.

--- src/run_pandoc.cmd
+++ src/run_pandoc.cmd
@@ -1,4 +1,4 @@
-pandoc --smart --atx-headers --from markdown --to markdown_mmd --reference-location section --metadata bibliography=literature.bib -o README_build.md README.md
+pandoc --smart --atx-headers --from markdown --to markdown_mmd --reference-location block --metadata bibliography=literature.bib -o README_build.md README.md
 REM --metadata link-citations 
 
 copy README_build.md ..\doc\README.md
